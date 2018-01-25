2e05f9a64b3a40971a1d9ca4577d618b95e8e6b5
RyunHwan <krh322@gmail.com>
2018-01-24 22:19:23 -0500

    change obout alarm that operating fan and heater and erase so many annotation(with Seung Soo)

--- client.py
+++ client.py
@@ -1,106 +1,87 @@
-import httplib
 import RPi.GPIO as GPIO
 import Adafruit_DHT as Adafruit_DHT
-import datetime
 import time
 import requests
 import urllib
 #import INA219
 
 sensor = Adafruit_DHT.DHT22
-wtime = datetime.datetime.now()
-GPIO.setmode(GPIO.BCM)
 
+GPIO.setmode(GPIO.BCM)
 #GPIO.setup(18,GPIO.OUT) #channel 1
 GPIO.setup(24,GPIO.OUT) #channel 2
 GPIO.setup(25,GPIO.OUT) #channel 3
 #GPIO.setup(27,GPIO.OUT) #channel 4
 GPIO.setup(3,GPIO.OUT) #DHT22
 
-#print r.status_code, r.reason
-#conn = httplib.HTTPConnection("192.168.2.148")
-#response = conn.getresponse()
-#status = response.status
-#print status
-#conn.request("HEAD","/")
-
-#All switch off before running
+##All switch off before running
 #GPIO.output(18,False)
 GPIO.output(24,False)
 GPIO.output(25,False)
 #GPIO.output(27,False)
 
-SHUNT_OHMS=0.1
-MAX_EXPECTED_AMPS=0.2
 cur_temperature=0.00
+bvolt=0.00
+ccurr=0.00
 
 def run():
-    global cur_temperature
+    global cur_temperature, bvolt, ccurr
+    is_fan_on = False
+    is_heater_on = False
     while(1):
+        now = time.localtime();
+        wtime = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, $
+        print(wtime)
         h,t = Adafruit_DHT.read_retry(sensor,20)
-        data = 'Temperature = {0:0.1f}*C Humidity = {1:0.1f}%'.format(t,h)
-        temperature = t
-        
-	URL = "http://192.168.2.52/test/waggle_receive.php"
+        data = 'Temperature = {0:0.1f} Humidity = {1:0.1f}'.format(t,h)
 
-	waggle_name = "Waggle21"
+        temperature = t
+        URL = "http://192.168.2.52/test/waggle_receive.php"
+
+        waggle_name = "Waggle1"
         battery = 10.5
         wind = 10.5
         solar = 10.5
-	PARAMS = {'waggle_name':waggle_name,'date':wtime, 'battery':battery,'wind':wind, 'solar':solar, 'temperature' : temperature, 'humidity' : h}
+        PARAMS = {'waggle_name':waggle_name,'date':wtime, 'battery':battery,'wind':wind, 'solar':solar$
 
         r= requests.post(url=URL,data=PARAMS)
-
         #measure_vol()
         print(wtime,data)
         if cur_temperature==temperature :
             continue
 
-        control_temperature(temperature)
-#       turn_on_fan(temperature)
+        is_fan_on, is_heater_on = control_temperature(temperature, is_fan_on, is_heater_on)
+
         cur_temperature=temperature
 
-def control_temperature(temp):
+def control_temperature(temp, f, h):
     print(temp)
-    ##Fan_Control##
-    if temp > 24.5 :
-        GPIO.output(25,True)
-    elif temp <= 15.0 :
-        GPIO.output(25,False)
-        
-    ##Heater_Control##
-    if temp < 10.0 :
-        GPIO.output(24,True)
-    elif temp >= 15.5 :
-        GPIO.output(24,False)
-        
-#def turn_on_fan(temp):
-#    print(temp)
-#    if temp > 30.0 :
-#        GPIO.output(25,True)
-#	print("turn on the fan")
-#    elif temp <= 30.0 :
-#        GPIO.output(25,False)
-#	h,t = Adafruit_DHT.read_retry(sensor,20)
-#	print("turn off the fan")
+    if f :
+        if temp < 24.0 :
+            print "turn off the Fan"
+            GPIO.output(25, False)
+            f = False
+    else :
+        if temp > 28.0 :
+            print "turn on the Fan"
+            GPIO.output(25, True)
+            f = True
 
-#def recharge():
- #       if voltage>13:
-           # GPIO.output(4,GPIO.low) 
-  #      if voltage<11.8:
-	   # GPIO.output(4,GPIO.high)
+    if h :
+        if temp > 24.0 :
+            print "turn off the Heatern"
+            GPIO.output(24, False)
+            h = False
+    else :
+        if temp < 20.0 :
+            print "turn on the Heater"
+            GPIO.output(24, True)
+            h = True
 
-SHUNT_OHMS = 0.1 
-MAX_EXPECTED_AMPS = 0.2 
+    return f, h
 
-#def measure_ele():
-#     ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS)
-#     ina.configure(ina.RANGE_16V)
-#     print "Bus Voltage: %.3f V" % ina.voltage()
-#     print "Bus Current: %.3f mA" % ina.current()
-#     print "Power: %.3f mW" % ina.power()
-#     print "Shunt voltage: %.3f mV" % ina.shunt_voltage()    
+SHUNT_OHMS = 0.1
+MAX_EXPECTED_AMPS = 2.0
 
 if __name__ == '__main__':
     run()
-
