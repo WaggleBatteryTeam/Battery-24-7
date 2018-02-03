e559e51bd1e11ad7fa8233d1b61be3b4ff0b1f10
gommuluk <skagbghk@gmail.com>
2018-02-02 20:01:02 -0800

    combine 'dbconnect_fcm.php' and '~_waggle.php'

---
+++ server/db_connection.php
@@ -0,0 +1,32 @@
+
+<?php
+	define("GOOGLE_API_KEY", "AAAAZsW0ktQ:APA91bGJexREXtZxuXR1vCxbPu63OUOsLeVZmB0WjviBIxwHbSiMgZKtyADfSKBfoP5Zrbot0AWSOQjWgaQ5fYUk5dvKQXkIZdV2lobsT0NSHlHpOC20gSPDuGOgr2zjwptMHRCImjlq");
+
+	function fn_db_connection($wanted_db)
+	{
+		if (strcmp($wanted_db, 'waggle')){
+			$dbhost = 'localhost';
+			$dbuser = 'root';
+			$dbpass = 'waggle';
+			$dbname = 'waggle';
+			
+		} elseif (strcmp($wanted_db, 'fcm')) {
+			$dbhost = 'localhost';
+			$dbuser = 'root';
+			$dbpass = 'waggle';
+			$dbname = 'fcm';
+
+		} else {
+			echo "NO DATABASE";
+		}
+
+		$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+		
+		if(!$conn) {
+			die("could not connect:" . mysql_error($conn));
+		}
+
+		return $conn
+	}
+
+?>
