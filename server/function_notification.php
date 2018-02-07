e64338780812d427736624d7a334a089265086a8
gommuluk <skagbghk@gmail.com>
2018-02-07 12:34:46 -0800

    seperate send_notification function to another php file

---
+++ server/function_notification.php
@@ -0,0 +1,33 @@
+
+<?php
+	function send_notification ($tokens, $waggle_id, $remain_battery)
+    {
+		$url = 'https://fcm.googleapis.com/fcm/send';
+		$fields = array(
+			'registration_ids' => $tokens,
+			'notification' => array('title' => 'Alert!', 'body' => $waggle_id.' has only '.$remain_battery.'%!!')
+			//'data' => array('message' => $message)
+		);
+
+		$headers = array(
+        		'Authorization:key =' . GOOGLE_API_KEY,
+        		'Content-Type: application/json'
+        	);
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+		$result = curl_exec($ch);
+		if ($result === FALSE) {
+ 		       	die('Curl failed: ' . curl_error($ch));
+		}
+		curl_close($ch);
+		return $result;
+	}
+
+?>
